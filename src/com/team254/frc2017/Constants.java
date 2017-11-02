dbd24c307408e6ac6b2211ec64c1146fe34d7b83
SuperMario210 <Sup3rMar1o210@gmail.com>
2017-11-02 15:19:11 -0700

    test

--- src/com/team254/frc2017/Constants.java
+++ src/com/team254/frc2017/Constants.java
@@ -19,28 +19,6 @@ import java.util.HashMap;
 public class Constants extends ConstantsBase {
     public static double kLooperDt = 0.005;
 
-    // Target parameters
-    // Source of current values: https://firstfrc.blob.core.windows.net/frc2017/Manual/2017FRCGameSeasonManual.pdf
-    // Section 3.13
-    // ...and https://firstfrc.blob.core.windows.net/frc2017/Drawings/2017FieldComponents.pdf
-    // Parts GE-17203-FLAT and GE-17371 (sheet 7)
-    public static double kPegTargetBottomHeight = 10.75;
-    public static double kGearTurnDistance = 24.0;
-    public static double kGearPlacementDistance = 10;//48.0;
-    public static double kGearPlacementSpeed = 60.0;
-    public static double kMinSpeed = 15.0;
-    public static double kGearMinLookAhead = 16.0; // inches
-    public static double kGearMinLookAheadSpeed = 9.0; // inches per second
-    public static double kGearMaxLookAhead = 22.0; // inches
-    public static double kGearMaxLookAheadSpeed = 120.0; // inches per second
-
-    // Shooter tuning parameters
-    public static boolean kIsShooterTuning = false;
-    public static double kShooterTuningRpmFloor = 2900;
-    public static double kShooterTuningRpmCeiling = 3500;
-    public static double kShooterTuningRpmStep = 50;
-    public static double kShooterTuningRpm = 3500.0;
-
     /* ROBOT PHYSICAL CONSTANTS */
 
     // Wheels
@@ -54,103 +32,8 @@ public class Constants extends ConstantsBase {
     public static double kCenterToRearBumperDistance = 16.99;
     public static double kCenterToSideBumperDistance = 17.225;
 
-    // Shooting suggestions
-    public static double kOnTargetErrorThreshold = 3.0;
-
-    // Intake Voltages
-    public static double kIntakeVoltageMax = 7.5;
-    public static double kIntakeVoltageMin = 5.5;
-    public static double kIntakeShootingVoltage = 4.0;
-    public static final double kIntakeVoltageDifference = kIntakeVoltageMax - kIntakeVoltageMin;
-
-    /* CONTROL LOOP GAINS */
-
-    // PID gains for drive velocity loop (HIGH GEAR)
-    // Units: setpoint, error, and output are in inches per second.
-    public static double kDriveHighGearVelocityKp = 0.8;
-    public static double kDriveHighGearVelocityKi = 0.0;
-    public static double kDriveHighGearVelocityKd = 6.0;
-    public static double kDriveHighGearVelocityKf = .15;
-    public static int kDriveHighGearVelocityIZone = 0;
-    public static double kDriveHighGearVelocityRampRate = 240.0;
-    public static double kDriveHighGearNominalOutput = 0.5;
-    public static double kDriveHighGearMaxSetpoint = 17.0 * 12.0; // 17 fps
-
-    // PID gains for drive velocity loop (LOW GEAR)
-    // Units: setpoint, error, and output are in inches per second.
-    public static double kDriveLowGearPositionKp = 1.0;
-    public static double kDriveLowGearPositionKi = 0.002;
-    public static double kDriveLowGearPositionKd = 100.0;
-    public static double kDriveLowGearPositionKf = .45;
-    public static int kDriveLowGearPositionIZone = 700;
-    public static double kDriveLowGearPositionRampRate = 240.0; // V/s
-    public static double kDriveLowGearNominalOutput = 0.5; // V
-    public static double kDriveLowGearMaxVelocity = 6.0 * 12.0 * 60.0 / (Math.PI * kDriveWheelDiameterInches); // 6 fps
-                                                                                                               // in RPM
-    public static double kDriveLowGearMaxAccel = 18.0 * 12.0 * 60.0 / (Math.PI * kDriveWheelDiameterInches); // 18 fps/s
-                                                                                                             // in RPM/s
 
     public static double kDriveVoltageCompensationRampRate = 0.0;
-
-    // Turn to heading gains
-    public static double kDriveTurnKp = 3.0;
-    public static double kDriveTurnKi = 1.5;
-    public static double kDriveTurnKv = 0.0;
-    public static double kDriveTurnKffv = 1.0;
-    public static double kDriveTurnKffa = 0.0;
-    public static double kDriveTurnMaxVel = 360.0;
-    public static double kDriveTurnMaxAcc = 720.0;
-
-    // Shooter gains
-    public static double kShooterTalonKP = 0.16;
-    public static double kShooterTalonKI = 0.00008;
-    public static double kShooterTalonKD = 0.0;
-    public static double kShooterTalonKF = 0.035;
-    public static double kShooterRampRate = 60.0;
-
-    public static double kShooterTalonHoldKP = 0.0;
-    public static double kShooterTalonHoldKI = 0.0;
-    public static double kShooterTalonHoldKD = 0.0;
-
-    public static double kShooterHoldRampRate = 720.0;
-
-    public static int kShooterTalonIZone = 1000;// 73 rpm
-    public static int kShooterOpenLoopCurrentLimit = 35;
-
-    public static double kShooterSetpointDeadbandRpm = 1.0;
-
-    // Used to determine when to switch to hold profile.
-    public static double kShooterMinTrackStability = 0.25;
-    public static double kShooterStartOnTargetRpm = 50.0;
-    public static double kShooterStopOnTargetRpm = 150.0;
-    public static int kShooterKfBufferSize = 20;
-    public static int kShooterMinOnTargetSamples = 20; // Should be <= kShooterKvBufferSize
-
-    public static int kShooterJamBufferSize = 30;
-    public static double kShooterDisturbanceThreshold = 25;
-    public static double kShooterJamTimeout = 1.5; // In secs
-    public static double kShooterUnjamDuration = 0.5; // In secs
-    public static double kShooterMinShootingTime = 1.0; // In secs
-
-    public static double kShooterSpinDownTime = 0.25;
-
-    // Feeder gains
-    public static double kFeederKP = 0.02;
-    public static double kFeederKI = 0.0;
-    public static double kFeederKD = 0.2;
-    public static double kFeederKF = 0.009;
-    public static double kFeederRampRate = 240.0;
-    public static double kFeederVoltageCompensationRampRate = 10.0;
-    public static double kFeederFeedSpeedRpm = 5400.0;
-    public static double kFeederSensorGearReduction = 3.0;
-
-    // Hopper gains
-    public static double kHopperRampRate = 48.0;
-
-    // Do not change anything after this line unless you rewire the robot and
-    // update the spreadsheet!
-    // Port assignments should match up with the spreadsheet here:
-    // https://docs.google.com/spreadsheets/d/12_Mrd6xKmxCjKtsWNpWZDqT7ukrB9-1KKFCuRrO4aPM/edit#gid=0
 
     /* TALONS */
     // (Note that if multiple talons are dedicated to a mechanism, any sensors
@@ -159,8 +42,8 @@ public class Constants extends ConstantsBase {
     // Drive
     public static final int kLeftDriveMasterId = 12;
     public static final int kLeftDriveSlaveId = 11;
-    public static final int kRightDriveMasterId = 5;
-    public static final int kRightDriverSlaveId = 2;
+    public static final int kRightDriveMasterId = 3;
+    public static final int kRightDriverSlaveId = 4;
 
     // Feeder
     public static final int kFeederMasterId = 8;
@@ -175,8 +58,8 @@ public class Constants extends ConstantsBase {
     public static final int kHopperSlaveId = 9;
 
     // Shooter
-    public static final int kRightShooterMasterId = 4;
-    public static final int kRightShooterSlaveId = 3;
+    public static final int kRightShooterMasterId = 2;
+    public static final int kRightShooterSlaveId = 1;
     public static final int kLeftShooterSlave1Id = 13;
     public static final int kLeftShooterSlave2Id = 14;
 
@@ -193,16 +76,16 @@ public class Constants extends ConstantsBase {
     public static int kLEDOnId = 2;
 
     // Digital Outputs
-    public static int kGreenLEDId = 0;
+    public static int kGreenLEDId = 9;
     public static int kRangeLEDId = 8;
 
     // Phone
     public static int kAndroidAppTcpPort = 8254;
 
     // Path following constants
-    public static double kMinLookAhead = 24.0; // inches
+    public static double kMinLookAhead = 12.0; // inches
     public static double kMinLookAheadSpeed = 9.0; // inches per second
-    public static double kMaxLookAhead = 36.0; // inches
+    public static double kMaxLookAhead = 24.0; // inches
     public static double kMaxLookAheadSpeed = 120.0; // inches per second
     public static double kDeltaLookAhead = kMaxLookAhead - kMinLookAhead;
     public static double kDeltaLookAheadSpeed = kMaxLookAheadSpeed - kMinLookAheadSpeed;
@@ -213,14 +96,14 @@ public class Constants extends ConstantsBase {
     public static double kSegmentCompletionTolerance = 0.1; // inches
     public static double kPathFollowingMaxAccel = 120.0; // inches per second^2
     public static double kPathFollowingMaxVel = 120.0; // inches per second
-    public static double kPathFollowingProfileKp = 0.25;
-    public static double kPathFollowingProfileKi = 0.1;
-    public static double kPathFollowingProfileKv = 0.0;
+    public static double kPathFollowingProfileKp = 5.00;
+    public static double kPathFollowingProfileKi = 0.03;
+    public static double kPathFollowingProfileKv = 0.02;
     public static double kPathFollowingProfileKffv = 1.0;
-    public static double kPathFollowingProfileKffa = 0.0;
-    public static double kPathFollowingGoalPosTolerance = 1.00;
+    public static double kPathFollowingProfileKffa = 0.05;
+    public static double kPathFollowingGoalPosTolerance = 0.75;
     public static double kPathFollowingGoalVelTolerance = 12.0;
-    public static double kPathStopSteeringDistance = 0.0;
+    public static double kPathStopSteeringDistance = 9.0;
 
     // Goal tracker constants
     public static double kMaxGoalTrackAge = 1.0;
@@ -232,8 +115,8 @@ public class Constants extends ConstantsBase {
     // Pose of the camera frame w.r.t. the robot frame
     public static double kCameraXOffset = -3.3211;
     public static double kCameraYOffset = 0.0;
-    public static double kCameraZOffset = 20.0; //height
-    public static double kCameraPitchAngleDegrees = 0; // Measured on 4/26
+    public static double kCameraZOffset = 20.9;
+    public static double kCameraPitchAngleDegrees = 29.56; // Measured on 4/26
     public static double kCameraYawAngleDegrees = 0.0;
     public static double kCameraDeadband = 0.0;
 
--- src/com/team254/frc2017/ControlBoard.java
+++ src/com/team254/frc2017/ControlBoard.java
@@ -9,18 +9,14 @@ import edu.wpi.first.wpilibj.Joystick;
  * 
  * @see ControlBoardInterface.java
  */
-public class ControlBoard implements ControlBoardInterface {
-    private static ControlBoardInterface mInstance = null;
+public class ControlBoard {
+    private static ControlBoard mInstance = null;
 
     private static final boolean kUseGamepad = false;
 
-    public static ControlBoardInterface getInstance() {
+    public static ControlBoard getInstance() {
         if (mInstance == null) {
-            if (kUseGamepad) {
-                mInstance = new GamepadControlBoard();
-            } else {
-                mInstance = new ControlBoard();
-            }
+            mInstance = new ControlBoard();
         }
         return mInstance;
     }
@@ -36,27 +32,26 @@ public class ControlBoard implements ControlBoardInterface {
     }
 
     // DRIVER CONTROLS
-    @Override
     public double getThrottle() {
         return -mThrottleStick.getRawAxis(0);
     }
 
-    @Override
+    
     public double getTurn() {
         return -mTurnStick.getY();
     }
 
-    @Override
+    
     public boolean getQuickTurn() {
         return mTurnStick.getRawButton(1);
     }
 
-    @Override
+    
     public boolean getLowGear() {
         return mThrottleStick.getRawButton(2);
     }
 
-    @Override
+    
     public boolean getAimButton() {
         return mTurnStick.getRawButton(2);
     }
@@ -66,82 +61,66 @@ public class ControlBoard implements ControlBoardInterface {
     }
 
     // OPERATOR CONTROLS
-    @Override
     public boolean getFeedButton() {
         return mButtonBoard.getRawAxis(1) < -0.1;
     }
-
-    @Override
+    
     public boolean getIntakeButton() {
         return mButtonBoard.getRawAxis(2) < -0.1;
     }
-
-    @Override
+    
     public boolean getShooterOpenLoopButton() {
         return false;
     }
-
-    @Override
+    
     public boolean getExhaustButton() {
         return mButtonBoard.getRawAxis(0) < -0.1;
     }
-
-    @Override
+    
     public boolean getLeftGearButton() {
         return mButtonBoard.getRawButton(4);
     }
-
-    @Override
+    
     public boolean getShooterClosedLoopButton() {
         return mButtonBoard.getRawButton(8);
     }
-
-    @Override
+    
     public boolean getFlywheelSwitch() {
         return mButtonBoard.getRawAxis(3) < -0.1;
     }
-
-    @Override
+    
     public boolean getHangButton() {
         return mButtonBoard.getRawButton(10);
     }
-
-    @Override
+    
     public boolean getGrabGearButton() {
         return mButtonBoard.getRawButton(2);
     }
-
-    @Override
+    
     public boolean getScoreGearButton() {
         return mButtonBoard.getRawButton(1);
     }
-
-    @Override
+    
     public boolean getActuateHopperButton() {
         return mButtonBoard.getRawButton(3);
     }
 
-    @Override
     public boolean getBlinkLEDButton() {
         return mButtonBoard.getRawButton(9);
     }
 
-    @Override
     public boolean getRangeFinderButton() {
         return mButtonBoard.getRawButton(7);
     }
-
-    @Override
+    
     public boolean getWantGearDriveLimit() {
         return mButtonBoard.getRawButton(12);
     }
-
-    @Override
+    
     public boolean getCenterGearButton() {
         return mButtonBoard.getRawButton(5);
     }
-
-    @Override
+    
     public boolean getRightGearButton() {
         return mButtonBoard.getRawButton(6);
     }
